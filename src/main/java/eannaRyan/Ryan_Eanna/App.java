69b10c7f2c6c0ed1b9dd1a729b61e9e03faa3149
eannaryan106 <34215963+eannaryan106@users.noreply.github.com>
2018-02-14 18:30:47 +0000

    App initial changes

--- src/main/java/eannaRyan/Ryan_Eanna/App.java
+++ src/main/java/eannaRyan/Ryan_Eanna/App.java
@@ -1,13 +1,51 @@
 package eannaRyan.Ryan_Eanna;
 
-/**
- * Hello world!
+
+import java.util.Date;
+import java.util.Scanner;
+/**********************************
+ * Date 2018	
+ * Author: COR
  *
- */
+********************************** */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String args[] )
     {
-        System.out.println( "Hello World!" );
+        App anApp= new App();
     }
+    // DATA
+    //.................
+    // declare objects
+    private Scanner someInput;
+    private Date today;
+    
+    //CONSTRUCTORS
+    //................
+    public App()
+    {
+    	//initialize variables
+    	
+    	//create objects
+    	this.someInput = new Scanner(System.in);
+    	
+    	//Starting Method
+    	private void sayHi()
+    	{
+    		this.today = new Date();
+    		System.out.println("App say Hi at " +today);
+    	}
+    	
+    	//pause before exit (this is only useful if an error occurs)
+    	System.out.println("\n Press enter to exit the program");
+    	this.someInput.nextLine();
+    	
+    	//close the program without error
+    	System.exit(0);
+    }
+
+	private void sayHi() {
+		// TODO Auto-generated method stub
+		
+	}
 }
