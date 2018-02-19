f321570190cc6a6c4d347a00d9811a9975193080
eannaryan106 <34215963+eannaryan106@users.noreply.github.com>
2018-02-19 18:33:13 +0000

    jOpt parameters(not added yet)

---
+++ run.bat
@@ -0,0 +1,10 @@
+@ECHO	OFF
+REM	---------------------------------------------------------------------
+REM	the	directory	path	to	java.exe	on	your	machine	may	be	different
+REM	from	the	path	I	have	between	the	double	quotes	below	to	this	may
+REM	need	to	be	changed	in	order	for	compile	to	work	on	your	machine
+REM	IF	JAVA_HOME	is	set	you	do	not	need	to	specify	the	path	just	java
+REM	---------------------------------------------------------------------
+
+C:\_DEV\Music_Pro>java -cp original-Music_Pro-v1.jar eannaRyan.Music_Pro.App
+PAUSE
--- src/main/java/eannaRyan/Music_Pro/App.java
+++ src/main/java/eannaRyan/Music_Pro/App.java
@@ -1,127 +1,181 @@
 package eannaRyan.Music_Pro;
 
-
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
-import	org.apache.logging.log4j.LogManager;
-import	org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
-//This	is	added	to	every	class	that	needs	to	log	with one	change
- //	The	getLogger(	)	part	should	contain	the	name	of	the	class	its	in
-
-/**********************************
- * Date 2018	
- * Author: COR edited as needed by ER
+/*****************************************************************
  *
+ * Date: 2017
+ * 
+ * @author COR
  *
- *	*	-	Demonstrates	the	use	of	development	tools	:	GIT,	MAVEN,	Eclipse
-	*	-	Demonstrates	how	to	use	Eclipse
-	*	-	Provides	a	refresher	of	OOP	in	Java
-	*	-	Provide	an	introduction	to	project	file	structure	layout	-	MAVEN	Archetype
-	*	-	Show	how	to	setup	Log4j2
-********************************** */
-public class App 
-{
-    public static void main( String args[] )
-    {
-              
-        //	To	view	the	arguments	being	entered
-	 	 seeCommandlineInput(args);
-			
-	 	 //	To	instantiate	App	class	based	in	the	parameters	entered	at	the	commandline
-	 actionCommandlineInput(args);
-    }
-    // DATA
-    //.................
-    // declare objects
-    private Scanner someInput;
-    private Date today;
-    private	static	Logger	LOG; 
-    
-    //CONSTRUCTORS
-    //................
-    public App()
-    {
-    	//initialize variables
-    	
-    	//create objects
-    	this.someInput = new Scanner(System.in);
-    	
-    	//Starting Method
-    	
-    	//associate	logging	with	this	class	so	know	the	messages	that	came	from	objects	of	this	class
-    	
-    	
-    	    	
-    	//private void sayHi()
-    	
-    	
-    		LOG	=	LogManager.getLogger(App.class);
-    		//log	tester
-         	testLogOutput();
-         	this.someInput.nextLine();
-    		this.today = new Date();
-    		System.out.println("App say Hi at " +today);
-    	    	     	
-    	//pause before exit (this is only useful if an error occurs)
-    	System.out.println("\n Press enter to exit the program");
-    	this.someInput.nextLine();
-    	
-    	//close the program without error
-    	System.exit(0);
-    }
+ * 
+ *         The purpose of this application is to provide an example for the
+ *         following:
+ * 
+ *         - Demonstrates the use of development tools : GIT, MAVEN, Eclipse -
+ *         Demonstrates how to use Eclipse - Provides a refresher of OOP in Java
+ *         - Provide an introduction to project file structure layout - MAVEN
+ *         Archetype - Show how to setup Log4j2
+ * 
+ *****************************************************************/
+public class App {
+	public static void main(String[] args) {
+		// To view the arguments being entered
+		seeCommandlineInput(args);
 
-		// TODO Auto-generated method stub
-		
-//		METHODS	used	by	main()	or	debug	methods	-	note	they	are	static	methods
-		 //............................................................
-		 /**
-		 	*	action	the	arguments	presented	at	the	command	line
-		 	*	instantiate	the	App	class	based	on	the	arguments	passed
-		 	*/
-		 	private	static	void	actionCommandlineInput(	String	args[]	)
-		 	{
-		 	 	//	no	special	instantiation	yet	as	don't	pass	args	to	it
-		 	 	App	anApp	=	new	App();
-		 	}
-					
-		 /**
-		 	*	View	the	arguments	presented	at	the	commandline
-		 	*	This	is	for	debug	and	demo	purposes
-		 	*/
-		 	private	static	void	seeCommandlineInput(	String	args[]	)
-		 	{	
-		 	 if	(args.length	==	0)
-									{
-													System.out.println("There	were	no	commandline	arguments	passed!");
-									}
-		 	 else
-			{
-		 	 	 //	display	the	command	line		entered	
-		 	 	 for(int	i	=	0;	i	<	args.length;	i++)	
-				{
-		 												System.out.println(args[i]);
-		 												
-		 								}
-			}
-				
-		 	}//EOM
-		 	
-		 /**
-		 	*	Test	the	Log4J2	logging
-		 	*/
-		 	private	static	void	testLogOutput()
-		 	{
-		 	 LOG.debug("Log	test:	Test	printed	on	debug");
-		 				LOG.info("Log	test:	Test	printed	on	info");
-		 				LOG.warn("Log	test:	Test	printed	on	warn");
-		 				LOG.error("Log	test:	Test	printed	on	error");
-		 				LOG.fatal("Log	test:	Test	printed	on	fatal");
-		 				LOG.info("Appending	string:	{}.",	"Application	log	test	message	-	Hi");
-				
-		 	}//EOM
-					
-	//EOC
-		
+		// To instantiate App class based in the parameters entered at the commandline
+		actionCommandlineInput(args);
 	}
 
+	// DATA
+	// ............................................................
+	// define attributes
+	private Scanner someInput;
+	private Date today;
+	// This is added to every class that needs to log with one change
+	// The getLogger( ) part should contain the name of the class its in
+	private static Logger LOG;
+
+	// CONSTRUCTORS
+	// ............................................................
+	public App(Level logLevel) {
+		// associate logging with this class so know the messages that came from objects
+		// of this cla
+		ss LOG = LogManager.getLogger(App.class);
+		Configurator.setLevel(LOG.getName(), logLevel);
+
+		// Check the log level requested
+		LOG.info("Commandline	requested	log	level:" + logLevel);
+		LOG.info("Application	started	with	log	level	debug:" + LOG.isDebugEnabled());
+
+		// test the logging
+		testLogOutput();
+
+		this.someInput = new Scanner(System.in);
+
+		// do something here
+		System.out.println("	\n	Soon	...	stuff	will	happen	here");
+
+		// pause before exit (this is only useful if an error occurs)
+		System.out.println("	\n	Press	enter	to	exit	the	program");
+		this.someInput.nextLine();
+		// close the program without error
+		System.exit(0);
+	}
+
+	public App() {
+		this(Level.INFO);
+	}
+
+	// METHODS used by main() or debug methods - note they are static methods
+	// ............................................................
+	/**
+	 	*	action	the	arguments	presented	at	the	command	line
+	 	*	instantiate	the	App	class	based	on	the	arguments	passed
+	 	*/
+	 	private	static	void	actionCommandlineInput(	String	args[]	)
+	 	{
+	 	 	try
+	 	 	 {	
+	 	 	 	 final	OptionParser	optionParser	=	new	OptionParser();
+				
+	 	 	 	 //define	the	allowed	arguments
+	 	 	 	 optionParser.acceptsAll(Arrays.asList("v",	"verbose"),	"Set	logging	level	t
+o	DEBUG	to	see	all	levels	of	log	messages").forHelp();	 	 	
+	 	 	 	 optionParser.acceptsAll(Arrays.asList("h",	"help"),	"Display	help/usage	inf
+ormation").forHelp();
+	 	 	 	 optionParser.acceptsAll(Arrays.asList("r",	"version"),	"Display	program	ver
+sion	information").forHelp();
+				
+	 	 	 	 final	OptionSet	options	=	optionParser.parse(args);
+				
+	 	 	 	 if	(options.has("help"))
+				{
+	 	 	 	 	 System.out.println("This	program	takes	an	SQL	database	with	a	User	
+table	as	displays	the	users.");
+	 	 	 	 	 System.out.println("It	is	provided	as	an	example	for	teaching	Java	
+programming.");
+	 	 	 	 	 printUsage(optionParser);
+	 	 	 	 	 System.exit(0);
+				}
+				
+	 	 	 	 if	(options.has("version"))
+				{
+	 	 	 	 	 System.out.println("Pythia	version	0.3");
+	 	 	 	 	 System.exit(0);
+				}
+				
+	 	 	 			//	valid	input	so	start	the	program	with	the	name	of	the	database	file	to	use
+	 	 	 			if	(options.has("verbose")	)
+	 	 	 			{
+	 	 	 	 			Level	logLevel	=	Level.DEBUG;
+	 	 	 	 			System.out.println("RUN	WITH:	logging	level	requested:	"	+	logLevel);
+	 	 	 	 			App	anApp	=	new	App(logLevel);
+	 	 	 			}
+	 	 	 			else
+	 	 	 			{
+	 	 	 	 			System.out.println("RUN	WITH:	logging	level	requested:	"	+	Level.INFO);
+	 	 	 	 			App	anApp	=	new	App();
+	 	 	 			}
+			}
+	 								catch	(OptionException	argsEx)
+	 								{
+	 									 	 System.out.println("ERROR:	Arguments\\parameter	is	not	valid.	"	+	argsEx);
+	 								}
+	 	}// EOM
+
+	/**
+	 * Write help message to standard output using the provided instance of
+	 * {@code	OptionParser}.
+	 */
+	private static void printUsage(final OptionParser parser) {
+		try {
+			parser.printHelpOn(System.out);
+		} catch (IOException ioEx) {
+			// System.out.println("ERROR: Unable to print usage - " + ioEx);
+			LOG.error("ERROR:	Unable	to	print	usage	-	" + ioEx);
+		}
+	}// EOM
+
+	/**
+	 * View the arguments presented at the commandline This is for debug and demo
+	 * purposes
+	 */
+	private static void seeCommandlineInput(String args[]) {
+		if (args.length == 0) {
+			System.out.println("There	were	no	commandline	arguments	passed!");
+		} else {
+			// display the command line entered
+			for (int i = 0; i < args.length; i++) {
+				System.out.println(args[i]);
+
+			}
+		}
+
+	}// EOM
+
+	/**
+	 * Test the Log4J2 logging
+	 */
+	private static void testLogOutput() {
+		LOG.debug("Log	test:	Test	printed	on	debug");
+		LOG.info("Log	test:	Test	printed	on	info");
+		LOG.warn("Log	test:	Test	printed	on	warn");
+		LOG.error("Log	test:	Test	printed	on	error");
+		LOG.fatal("Log	test:	Test	printed	on	fatal");
+		LOG.info("Appending	string:	{}.", "Application	log	test	message	-	Hi");
+
+	}// EOM
+
+}// EOC
